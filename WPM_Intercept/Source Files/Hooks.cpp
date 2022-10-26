6f3e9fc562cc0fe4d33b266db0095dd40f0199d5
Joseph Kerr <114572797+altoid29@users.noreply.github.com>
2022-10-26 17:41:33 -0400

    Update Hooks.cpp

--- WPM_Intercept/Source Files/Hooks.cpp
+++ WPM_Intercept/Source Files/Hooks.cpp
@@ -3,7 +3,8 @@
 
 // --------------------------------------------------------------------------------------------------------------------------
 
-BOOL WINAPI HookRelated::WriteProcessMemoryDetour(HANDLE hProcess, LPVOID lpBaseAddress, LPCVOID lpBuffer, SIZE_T nSize, SIZE_T* lpNumberOfBytesWritten) {
+BOOL WINAPI HookRelated::WriteProcessMemoryDetour(HANDLE hProcess, LPVOID lpBaseAddress, LPCVOID lpBuffer, SIZE_T nSize, SIZE_T* lpNumberOfBytesWritten)
+{
     // File name which is set to the address of the dumped address.
     char buffer[9999];
     sprintf_s(buffer, "%s\\WPM_0x%p%s", DIRECTORY, lpBaseAddress, ".bin");
@@ -11,10 +12,13 @@ BOOL WINAPI HookRelated::WriteProcessMemoryDetour(HANDLE hProcess, LPVOID lpBase
     // Create a file and use the name from the buffer.
     const HANDLE hFile = CreateFile(buffer, GENERIC_READ | GENERIC_WRITE, NULL, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
 
-    if (hFile != nullptr) {
-        if (lpBaseAddress != nullptr && nSize > 8 && lpBuffer > 0x0) {
+    if (hFile != nullptr)
+    {
+        if (lpBaseAddress != nullptr && nSize > 8 && lpBuffer > 0x0)
+        {
             // Write the content to the file with WriteFile.
-            try {
+            try
+            {
 #ifdef _WIN64
                 WriteFile(hFile, lpBuffer, nSize, reinterpret_cast<LPDWORD>(lpNumberOfBytesWritten), nullptr);
 #else
@@ -22,7 +26,8 @@ BOOL WINAPI HookRelated::WriteProcessMemoryDetour(HANDLE hProcess, LPVOID lpBase
 #endif
                 std::cout << "[WPM] Wrote " << nSize << " bytes (0x" << std::hex << nSize << ")" << std::dec << " from address " << "0x" << lpBaseAddress << std::endl;
             }
-            catch (std::exception& error) {
+            catch (std::exception& error)
+            {
 
             }
 
@@ -39,7 +44,8 @@ BOOL WINAPI HookRelated::WriteProcessMemoryDetour(HANDLE hProcess, LPVOID lpBase
 
 // --------------------------------------------------------------------------------------------------------------------------
 
-BOOL NTAPI HookRelated::NtWriteVirtualMemoryDetour(HANDLE ProcessHandle, PVOID BaseAddress, PVOID Buffer, ULONG BufferSize, PULONG NumberOfBytesWritten) {
+BOOL NTAPI HookRelated::NtWriteVirtualMemoryDetour(HANDLE ProcessHandle, PVOID BaseAddress, PVOID Buffer, ULONG BufferSize, PULONG NumberOfBytesWritten)
+{
     // File name with the current address.
     char buffer[9999];
     sprintf_s(buffer, "%s\\NT_0x%p%s", DIRECTORY, BaseAddress, ".bin");
@@ -47,9 +53,12 @@ BOOL NTAPI HookRelated::NtWriteVirtualMemoryDetour(HANDLE ProcessHandle, PVOID B
     // Create a file and use the name from the buffer.
     const HANDLE hFile = CreateFile(buffer, GENERIC_READ | GENERIC_WRITE, NULL, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
 
-    if (hFile != nullptr) {
-        if (BaseAddress != nullptr && BufferSize > 8 && Buffer > 0x0) {
-            try {
+    if (hFile != nullptr)
+    {
+        if (BaseAddress != nullptr && BufferSize > 8 && Buffer > 0x0)
+        {
+            try
+            {
 #ifdef _WIN64
                 // Casting needed?
                 WriteFile(hFile, Buffer, BufferSize, reinterpret_cast<LPDWORD>(NumberOfBytesWritten), nullptr);
@@ -59,17 +68,18 @@ BOOL NTAPI HookRelated::NtWriteVirtualMemoryDetour(HANDLE ProcessHandle, PVOID B
 
                 std::cout << "[NT] Wrote " << BufferSize << " bytes (0x" << std::hex << BufferSize << ")" << std::dec << " from address " << "0x" << BaseAddress << std::endl;
             }
-            catch (std::exception& error) {
+            catch (std::exception& error)
+            {
 
+            }
+
+            // Close the handle so we can open the files.
+            CloseHandle(hFile);
         }
+        else
+            std::cout << "[!] Invalid file handle (0x" << hFile << ")" << std::endl;
 
-        // Close the handle so we can open the files.
-        CloseHandle(hFile);
+        return originalNtWriteVirtualMemory(ProcessHandle, BaseAddress, Buffer, BufferSize, NumberOfBytesWritten);
     }
-    else
-        std::cout << "[!] Invalid file handle (0x" << hFile << ")" << std::endl;
 
-    return originalNtWriteVirtualMemory(ProcessHandle, BaseAddress, Buffer, BufferSize, NumberOfBytesWritten);
-}
-
-// --------------------------------------------------------------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------------------------------------
